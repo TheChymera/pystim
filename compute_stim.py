ee763edbf0c76abe2bc175ddd215200f0438de10
Horea Christian <h.chr@mail.ru>
2013-10-07 07:55:56 +0200

    Added support for differnt outputs (ongoing work) + now using config files

--- compute_stim.py
+++ compute_stim.py
@@ -6,33 +6,38 @@ import pandas as pd
 from numpy.random import permutation, choice, sample
 from os import path, listdir
 from itertools import product
-
-from lefunctions import save_pd_csv
+from data_functions import get_config_file, save_pd_csv, save_gen_csv
 from routines import harari 
 
-# General parameters:
-sequence_name = 'faceRT_blocksize4'
-output_subdir = 'sequences/'
-stimuli_dir = '~/src/faceRT/img/px0'
-scrambling_id = 'rand' #identifies scrambled files
-scrambling_a_id = '_a.j' #first scrambling versions
-scrambling_b_id = '_b.j' #second scrambling versions
-fearful_id = '_FE_' #fearful faces
-happy_id = '_HA_' #happy faces
-female_id = 'F_'
-male_id = 'M_'
-easy_em_id = 'C100'
-hard_em_id = 'C040'
-scrambling_steps_id = ['cell6', 'cell10', 'cell14', 'cell18', 'cell22']
-block_size = 4
+config = get_config_file()
 
+#IMPORT VARIABLES
+sequence_name = config.get('Files', 'sequence_name')
+output_subdir = config.get('Directories', 'output_subdir')
+stimuli_dir = config.get('Directories', 'stimuli_dir')
+templates_dir = config.get('Directories', 'templates_dir')
+scrambling_id = config.get('Decoding', 'scrambling_id')
+scrambling_a_id = config.get('Decoding', 'scrambling_a_id')
+scrambling_b_id = config.get('Decoding', 'scrambling_b_id')
+fearful_id = config.get('Decoding', 'fearful_id')
+happy_id = config.get('Decoding', 'happy_id')
+female_id = config.get('Decoding', 'female_id')
+male_id = config.get('Decoding', 'male_id')
+easy_em_id = config.get('Decoding', 'easy_em_id')
+hard_em_id = config.get('Decoding', 'hard_em_id')
+scrambling_steps_id = [int(i) for i in config.get('Decoding', 'scrambling_steps_id').split(',')]
+scrambling_steps_prefix = config.get('Decoding', 'scrambling_steps_prefix')
+make_blocks = config.getboolean('Parameters', 'make_blocks')
+block_size = config.getint('Parameters', 'block_size')
+output_format = config.get('Parameters', 'output_format')
+#END IMPORT VARIABLES
+
+scrambling_steps_id_withprefix = [scrambling_steps_prefix+str(i) for i in scrambling_steps_id]
 local_dir = path.dirname(path.realpath(__file__)) + '/' 
 output_dir = local_dir + output_subdir
-output_file = output_dir + sequence_name
 stimuli_dir = path.expanduser(stimuli_dir) # expands the path if it is specified with tilde for "home"
 stimlist = permutation(listdir(stimuli_dir))
-sequence = pd.DataFrame([]) # blank dataframe to add the stimuli lists to 
-
+sequence = pd.DataFrame([]) # blank dataframe to add the stimuli lists to
 
 # 100% emotion trials
 for variable_categories in product([male_id, female_id], [[happy_id, fearful_id],[fearful_id,happy_id]]):
@@ -50,7 +55,7 @@ for variable_categories in product([male_id, female_id], [[happy_id, fearful_id]
     sequence = pd.concat([sequence, subsequence], ignore_index=True)
     
 # scrambling trials
-for variable_categories in product(scrambling_steps_id, [[scrambling_a_id, scrambling_b_id]]):
+for variable_categories in product(scrambling_steps_id_withprefix, [[scrambling_a_id, scrambling_b_id]]):
     top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a]
     distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a]
     for idx, i in enumerate(distractors):
@@ -66,13 +71,30 @@ sequence.ix[(sequence['right face'].map(lambda x: easy_em_id in x)), 'emotion in
 sequence.ix[(sequence['right face'].map(lambda x: hard_em_id in x)), 'emotion intensity'] = 40 # the top face is always C100
 sequence.ix[(sequence['top face'].map(lambda x: male_id in x)), 'gender'] = 'm'
 sequence.ix[(sequence['top face'].map(lambda x: female_id in x)), 'gender'] = 'f'
-for scrambling_step in scrambling_steps_id:
+for scrambling_step in scrambling_steps_id_withprefix:
     sequence.ix[(sequence['top face'].map(lambda x: scrambling_step in x)), 'scrambling'] = int(scrambling_step.strip('cell'))
 sequence.ix[(sequence['top face'].map(lambda x: scrambling_id not in x)), 'scrambling'] = 0
 #make blocks
-block_number = 0 #start value for iteration
-for step in range(0, len(sequence)*block_size, block_size):
-    sequence.ix[step:step+block_size,'block'] = block_number
-    block_number +=1
+if make_blocks:
+    block_number = 0 #start value for iteration
+    for step in range(0, len(sequence)*block_size, block_size):
+        sequence.ix[step:step+block_size,'block'] = block_number
+        block_number +=1
+
+if output_format == 'christian':
+    output_file = output_dir + sequence_name
+    save_pd_csv(sequence, output_file)
+elif output_format == 'gabriela':
+    tamplate_subdir = output_format + '/'
+    header = open(local_dir+templates_dir+tamplate_subdir+'header.txt', 'r')
+    footer = open(local_dir+templates_dir+tamplate_subdir+'footer.txt', 'r')
+    module = open(local_dir+templates_dir+tamplate_subdir+'module.txt', 'r')
     
-save_pd_csv(sequence, output_file)
+    files_list = [easy_em_id, hard_em_id, ]
+    output_file = output_dir + sequence_name + '_' + output_format + '.csv'
+    with save_gen_csv(output_file) as outfile:
+        outfile.write(header.read())
+        
+        outfile.write(footer.read())
+
+
