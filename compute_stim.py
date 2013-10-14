51b6fa5f8eaa04bfba8583b3117c575c2f8ab8ff
Horea Christian <h.chr@mail.ru>
2013-10-14 13:04:37 +0200

    working on "just reexporting" functionality

--- compute_stim.py
+++ compute_stim.py
@@ -37,65 +37,73 @@ def main(output_format=False, scrambling_steps_id=False):
     if output_format:
         pass
     else: output_format = config.get('Parameters', 'output_format')
+    keep_oldsequence = config.getboolean('Parameters', 'keep_oldsequence')
     #END IMPORT VARIABLES
     
-    scrambling_steps_id_withprefix = [scrambling_steps_prefix+str(i) for i in scrambling_steps_id]
     local_dir = path.dirname(path.realpath(__file__)) + '/' 
     output_dir = local_dir + output_subdir
-    stimuli_dir = path.expanduser(stimuli_dir) # expands the path if it is specified with tilde for "home"
-    stimlist = permutation(listdir(stimuli_dir))
-    sequence = pd.DataFrame([]) # blank dataframe to add the stimuli lists to
-    if make_blocks:
-        sequence_name = sequence_name + '_' + str(block_size) + 'block'
-    
-    # 100% emotion trials
-    for variable_categories in product([male_id, female_id], [[happy_id, fearful_id],[fearful_id,happy_id]]):
-        top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a and scrambling_id not in a]
-        distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][1] in a and easy_em_id in a and scrambling_id not in a]
-        subsequence = hariri(top_stimuli, top_stimuli, distractors, suffix_characters=11) # with suffix identifier skipping because we don't want the same person twice in a slide
-        sequence = pd.concat([sequence, subsequence], ignore_index=True)
-    
-    # 40% emotion trials
-    for variable_categories in product([male_id, female_id], [[happy_id, fearful_id],[fearful_id,happy_id]]):
-        top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a and scrambling_id not in a]
-        distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][1] in a and hard_em_id in a and scrambling_id not in a]
-        targets = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and hard_em_id in a and scrambling_id not in a]
-        subsequence = hariri(top_stimuli, targets, distractors, suffix_characters=11) # with suffix identifier skipping because we don't want the same person twice in a slide
-        sequence = pd.concat([sequence, subsequence], ignore_index=True)
+
+    if keep_oldsequence:
+        if path.isfile(output_dir + '.' + sequence_name + 'last_exported_sequence'):
+            sequence = pd.from_csv(output_dir + '.' + sequence_name + 'last_exported_sequence.csv')
+        else: pass
+    else:
+        ### START CREATING THE NEW STIMLIST DATAFRAME
+        scrambling_steps_id_withprefix = [scrambling_steps_prefix+str(i) for i in scrambling_steps_id]
+        stimuli_dir = path.expanduser(stimuli_dir) # expands the path if it is specified with tilde for "home"
+        stimlist = permutation(listdir(stimuli_dir))
+        sequence = pd.DataFrame([]) # blank dataframe to add the stimuli lists to
+        if make_blocks:
+            sequence_name = sequence_name + '_' + str(block_size) + 'block'
         
-    # scrambling trials
-    for variable_categories in product(scrambling_steps_id_withprefix, [[scrambling_a_id, scrambling_b_id]]):
-        top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a]
-        distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a]
-        for idx, i in enumerate(distractors):
-            distractors[idx] = distractors[idx][:-5]+'b.jpg'
-        targets = top_stimuli # we use identical images for identification
-        subsequence = hariri(top_stimuli, targets, distractors, forbid_identical_targets=False)
-        sequence = pd.concat([sequence, subsequence], ignore_index=True)
-    
-    # Fill out meta-fileds
-    sequence.ix[(sequence['top face'].map(lambda x: happy_id in x)), 'emotion'] = 'happiness'
-    sequence.ix[(sequence['top face'].map(lambda x: fearful_id in x)), 'emotion'] = 'fear'
-    sequence.ix[(sequence['right face'].map(lambda x: easy_em_id in x)), 'emotion intensity'] = 100 # the top face is always C100
-    sequence.ix[(sequence['right face'].map(lambda x: hard_em_id in x)), 'emotion intensity'] = 40 # the top face is always C100
-    sequence.ix[(sequence['top face'].map(lambda x: male_id in x)), 'gender'] = 'm'
-    sequence.ix[(sequence['top face'].map(lambda x: female_id in x)), 'gender'] = 'f'
-    for scrambling_step in scrambling_steps_id_withprefix:
-        sequence.ix[(sequence['top face'].map(lambda x: scrambling_step in x)), 'scrambling'] = int(scrambling_step.strip('cell'))
-    sequence.ix[(sequence['top face'].map(lambda x: scrambling_id not in x)), 'scrambling'] = 0
-    
-    #make blocks
-    if make_blocks:
-        block_number = 0 #start value for iteration
-        for step in range(0, len(sequence)*block_size, block_size):
-            sequence.ix[step:step+block_size,'block'] = block_number
-            block_number +=1
+        # 100% emotion trials
+        for variable_categories in product([male_id, female_id], [[happy_id, fearful_id],[fearful_id,happy_id]]):
+            top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a and scrambling_id not in a]
+            distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][1] in a and easy_em_id in a and scrambling_id not in a]
+            subsequence = hariri(top_stimuli, top_stimuli, distractors, suffix_characters=11) # with suffix identifier skipping because we don't want the same person twice in a slide
+            sequence = pd.concat([sequence, subsequence], ignore_index=True)
+        
+        # 40% emotion trials
+        for variable_categories in product([male_id, female_id], [[happy_id, fearful_id],[fearful_id,happy_id]]):
+            top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a and scrambling_id not in a]
+            distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][1] in a and hard_em_id in a and scrambling_id not in a]
+            targets = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and hard_em_id in a and scrambling_id not in a]
+            subsequence = hariri(top_stimuli, targets, distractors, suffix_characters=11) # with suffix identifier skipping because we don't want the same person twice in a slide
+            sequence = pd.concat([sequence, subsequence], ignore_index=True)
+            
+        # scrambling trials
+        for variable_categories in product(scrambling_steps_id_withprefix, [[scrambling_a_id, scrambling_b_id]]):
+            top_stimuli = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a]
+            distractors = [a for a in stimlist if variable_categories[0] in a and variable_categories[1][0] in a and easy_em_id in a]
+            for idx, i in enumerate(distractors):
+                distractors[idx] = distractors[idx][:-5]+'b.jpg'
+            targets = top_stimuli # we use identical images for identification
+            subsequence = hariri(top_stimuli, targets, distractors, forbid_identical_targets=False)
+            sequence = pd.concat([sequence, subsequence], ignore_index=True)
+        
+        # Fill out meta-fileds
+        sequence.ix[(sequence['top face'].map(lambda x: happy_id in x)), 'emotion'] = 'happiness'
+        sequence.ix[(sequence['top face'].map(lambda x: fearful_id in x)), 'emotion'] = 'fear'
+        sequence.ix[(sequence['right face'].map(lambda x: easy_em_id in x)), 'emotion intensity'] = 100 # the top face is always C100
+        sequence.ix[(sequence['right face'].map(lambda x: hard_em_id in x)), 'emotion intensity'] = 40 # the top face is always C100
+        sequence.ix[(sequence['top face'].map(lambda x: male_id in x)), 'gender'] = 'm'
+        sequence.ix[(sequence['top face'].map(lambda x: female_id in x)), 'gender'] = 'f'
+        for scrambling_step in scrambling_steps_id_withprefix:
+            sequence.ix[(sequence['top face'].map(lambda x: scrambling_step in x)), 'scrambling'] = int(scrambling_step.strip('cell'))
+        sequence.ix[(sequence['top face'].map(lambda x: scrambling_id not in x)), 'scrambling'] = 0
+        
+        #make blocks
+        if make_blocks:
+            block_number = 0 #start value for iteration
+            for step in range(0, len(sequence)*block_size, block_size):
+                sequence.ix[step:step+block_size,'block'] = block_number
+                block_number +=1
             
     # BEGIN OUTPUT FILE FORMATTING
-    if output_format == 'christian': # 'christian' format (dataframe, versatile, amongst others for faceRT)
+    if 'christian' in output_format: # 'christian' format (dataframe, versatile, amongst others for faceRT)
         output_file = output_dir + sequence_name
         save_pd_csv(sequence, output_file)
-    elif output_format == 'gabriela': # 'gabriela' format (for presentation)
+    elif 'gabriela1' in output_format: # 'gabriela' format (for presentation)
         tamplate_subdir = output_format + '/'
         header = open(local_dir+templates_dir+tamplate_subdir+'header.txt', 'r').read()
         footer = open(local_dir+templates_dir+tamplate_subdir+'footer.txt', 'r').read()
@@ -120,6 +128,8 @@ def main(output_format=False, scrambling_steps_id=False):
                         format_module(outfile, module, trial, idx+1)
                 else: raise InputError('Your condition_file_id values do not correspond to the script\'s expectations.')
                 outfile.write(footer)
+    output_file = output_dir + '.' + sequence_name + 'last_exported_sequence'
+    save_pd_csv(sequence, output_file)
     # BEGIN OUTPUT FILE FORMATTING
 
 def format_module(outfile, module, trial, idx):
